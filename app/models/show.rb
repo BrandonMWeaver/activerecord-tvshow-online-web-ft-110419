6458565470ed0d925ea89ccee13f33f4fc8bd725
Brandon Weaver <a7xncob@gmail.com>
2019-12-13 22:09:42 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    return self.maximum("rating")
+  end
+  
+  def self.most_popular_show
+    return self.where("rating = #{self.highest_rating}").first
+  end
+  
+  def self.lowest_rating
+    return self.minimum("rating")
+  end
+  
+  def self.least_popular_show
+    return self.where("rating = #{self.lowest_rating}").first
+  end
+  
+  def self.ratings_sum
+    return self.sum("rating")
+  end
+  
+  def self.popular_shows
+    return self.where("rating > 5")
+  end
+  
+  def self.shows_by_alphabetical_order
+    return self.order("name")
+  end
+  
+end
